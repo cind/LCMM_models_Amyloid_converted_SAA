c26f79f838ed6e84a26ac98457671067777c1c58
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2023-12-13 15:27:46 -0800

    Update Example.R
    added an example of a batch variable

--- Example.R
+++ Example.R
@@ -1,1 +1,7 @@
 # Example
+data$batch <- case_when(data$ADNI_Phase == "ADNI1" & kdata$FIELD_STRENGTH == "1.5T" ~ "Protocol 1",
+                            data$ADNI_Phase == "ADNI2" & data$FIELD_STRENGTH == "1.5T" ~ "Protocol 1",
+                            data$ADNI_Phase == "ADNIGO" & data$FIELD_STRENGTH == "1.5T" ~ "Protocol 1",
+                            data$ADNI_Phase == "ADNI2" & data$FIELD_STRENGTH == "3T" ~ "Protocol 2",
+                            data$ADNI_Phase == "ADNIGO" & data$FIELD_STRENGTH == "3T" ~ "Protocol 2",
+                            data$ADNI_Phase == "ADNI3" & data$FIELD_STRENGTH == "3T" ~ "Protocol 3")
