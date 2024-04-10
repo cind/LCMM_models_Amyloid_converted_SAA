b052ff9fe63eaf1f98509b73b0d1ba638635174d
toomanycats <daniel.cuneo@ncire.org>
2024-04-10 10:32:39 -0700

    more comments.

--- create_r_environment.r
+++ create_r_environment.r
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
-# Be sure to first export RVERSION=4.3.2
-# Also, have an ~/.Rprofile with .libPaths("/home/vhasfccuneod/Renv")
+# Environment variables must be setup first.
+##
+# export RVERSION=4.3.2
+# export R_LIBS_USER=~/Renv
 
+
+# The library `parallel`, seems to be included in the base distribution and throws a warning when trying to install.
 # install the R packages, if nbt already installed
-for (package in c("plyr", "ggplot2", "segmented", "lcmm", "ggrepel", "parallel")) {
+for (package in c("plyr", "ggplot2", "segmented", "lcmm", "ggrepel")) {
           if (!require(package, character.only=TRUE, quietly=TRUE)) {
                             install.packages(package, repos = "http://cran.us.r-project.org", lib="/home/vhasfccuneod/Renv")
     library(package, character.only=TRUE, quietly=TRUE)
