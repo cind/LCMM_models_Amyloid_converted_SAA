0d9e8110f7e94a721e56d2158ed32468cc1cd4d7
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-04-12 10:20:03 -0700

    Update helper_functions_parallel.R
    trying to just get a dataframe with the values coming out of each iteration. I would rather do any kind of calculations for confidence intervals outside of this function.

--- helper_functions_parallel.R
+++ helper_functions_parallel.R
@@ -1,71 +1,39 @@
 ################################################################################################  Bootstrapping Function  #####################################################################################################
 library("parallel")
 
-lcmm_helper <- function(n_sample) {
+
+  # No parallel, test the code.
+  #lcmm_helper(new_data, n_sample, lcmm_data, name_of_biomarker, boot_pred, boot_derivs)
+  
+  # registerDoParallel(16)
+  # foreach (i=1:B) %dopar% {
+  #   lcmm_helper(i, new_data, n_sample, lcmm_data, name_of_biomarker, boot_pred, boot_derivs)
+  # }
+  
+  registerDoParallel(16)
+  foreach(ind = 1:B) %dopar% {
+    # ind = i
+    sprintf("%i:%s", ind, name_of_biomarker)
     boot <- sample(n_sample, n_sample, replace = TRUE)
-
+    
     # lcmm_data$biomarker <- lcmm_data[,"name_of_biomarker"]
-    lcmm_data$biomarker <- lcmm_data[,as.character(substitute(name_of_biomarker))]
-
-
-    fit.b <- lcmm(biomarker ~ adjusted_new_time + age + age*adjusted_new_time + PTGENDER + PTEDUCAT + apoe, #link = c("5-quantsplines"),
-                  random = ~adjusted_new_time, subject="RID", maxiter = 300, data = lcmm_data[boot,], link = "3-equi-splines")
-
-    # boot_predict_splines <- predictlink(fit.b)
-
-    boot_pred[i] <- predictY(fit.b, new_data, var.time = "adjusted_new_time", draws = TRUE)
-
-    boot_pred_fit <- as.data.frame(boot_pred[i])
-
-    boot_derivs[,i] <- diff(boot_pred_fit$Ypred_50)/diff(new_data$adjusted_new_time)
-
+    #lcmm_data$biomarker <- lcmm_data[ , as.character(substitute(name_of_biomarker))]
+    lcmm_data$biomarker <- lcmm_data[ , name_of_biomarker]
+    
+    fit.b <- lcmm::lcmm(biomarker ~ adjusted_new_time + age + age*adjusted_new_time + PTGENDER + PTEDUCAT + apoe, #link = c("5-quantsplines"),
+                        random = ~adjusted_new_time, subject="RID", maxiter = 300, data = lcmm_data[boot,], link = "3-equi-splines")
+    
+    boot_pred[ind] <- lcmm::predictY(fit.b, new_data, var.time = "adjusted_new_time", draws = TRUE)
+    boot_pred_fit <- as.data.frame(boot_pred[ind])
+    boot_derivs[,ind] <- diff(boot_pred_fit$Ypred_50)/diff(new_data$adjusted_new_time)
+    boot_derivs_for_bind <- append(boot_derivs[,ind], NA)
+    boot_derivs_for_bind$biomarker <- name_of_biomarker
+      
+    bootstrapped_list_values <- cbind(boot_pred_fit, boot_derivs_for_bind) 
   }
-
-lcmm_bootstrap_ci <- function(new_data, n_iterations, lcmm_data, name_of_biomarker) {
-  options(mc.cores = 8)
-
-  #prepping for bootstrapping
-  n_sample <- nrow(lcmm_data)
-  n_rows <- nrow(new_data) # newdata for predictions
-  B <- n_iterations
-  boot_derivs <- matrix(nrow = n_rows-1, ncol=B) #for the finite difference
-  boot_derivs <- as.data.frame(boot_derivs)
-  boot_pred <- numeric(B) #for the predictions
-
-  #making a for loop for the iterations
-  set.seed(121)
-  mclapply(seq(1, n_iterations), lcmm_helper, .pb = set_kpb)
-
-  boot_pred_data <- as.data.frame(boot_pred)
-  data_2.5 <- boot_pred_data[ , grepl( "Ypred_2.5" , names( boot_pred_data ) ) ]
-  data_97.5 <- boot_pred_data[ , grepl( "Ypred_97.5" , names( boot_pred_data ) ) ]
-  ci_2.5 <- rowMeans(data_2.5)
-  ci_97.5 <- rowMeans(data_97.5)
-
-  #getting bootstrapped ci's from predictions
-  data_preds <- boot_pred_data[ , grepl( "Ypred_50" , names( boot_pred_data ) ) ]
-  median_preds_ci_2.5 <- apply(as.matrix(data_preds), 1, function(x){median(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[1,]
-  median_preds_ci_97.5 <- apply(as.matrix(data_preds), 1, function(x){median(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[2,]
-  mean_pred_ci_2.5 <- apply(as.matrix(data_preds), 1, function(x){mean(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[1,]
-  mean_pred_ci_97.5 <- apply(as.matrix(data_preds), 1, function(x){mean(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[2,]
-
-
-  #now for the slopes
-  ci_diffs_2.5 <- append(apply(as.matrix(boot_derivs), 1, function(x){mean(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[1,], NA)
-  ci_diffs_97.5 <- append(apply(as.matrix(boot_derivs), 1, function(x){mean(x)+c(-1.96,1.96)*sd(x)/sqrt(length(x))})[2,], NA)
-
-  #now putting into a data frame
-  bootstrapped_data <- data.frame(new_data, ci_2.5, ci_97.5, ci_diffs_2.5, ci_diffs_97.5, median_preds_ci_2.5, median_preds_ci_97.5, mean_pred_ci_2.5, mean_pred_ci_97.5)
-
-  bootstrapped_data$signif <- case_when(bootstrapped_data$ci_diffs_2.5 > 0 & bootstrapped_data$ci_diffs_97.5 > 0 ~ "sig",
-                                        bootstrapped_data$ci_diffs_2.5 < 0 & bootstrapped_data$ci_diffs_97.5 < 0 ~ "sig",
-                                        TRUE ~ "not sig")
-
-  bootstrapped_data$biomarker <- name_of_biomarker
-
-  return(bootstrapped_data)
-
-}
+  
+  return(bootstrapped_list_values)
+  
 
 ################################################################################################  Smoothing Function  #####################################################################################################
 
