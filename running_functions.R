a4356094feebfdde25307fc64e0fde9dca8a026e
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-04-17 10:11:56 -0700

    Update running_functions.R
    changed source and data paths

--- running_functions.R
+++ running_functions.R
@@ -1,7 +1,7 @@
 library(lcmm)
-source("/home/vhasfccuneod/Biomarkers_bootstrap_parallel/Longitudinal-COMBAT-GAM-and-analysis-of-longitudinal-data/helper_functions_parallel.R")
+source("/home/vhasfcschaat/Longitudinal-COMBAT-GAM-and-analysis-of-longitudinal-data/helper_functions_parallel.R")
 
-root = "/home/vhasfccuneod/Biomarkers_bootstrap_parallel/Longitudinal-COMBAT-GAM-and-analysis-of-longitudinal-data/lcmm_modelling_data"
+root = "/home/vhasfcschaat/Longitudinal-COMBAT-GAM-and-analysis-of-longitudinal-data/lcmm_modelling_data"
 
 centiloid_plot_data <- read.csv(file.path(root, "centiloid_lcmm_data.csv"))
 tau_plot_data <- read.csv(file.path(root, "tau_lcmm_data.csv"))
