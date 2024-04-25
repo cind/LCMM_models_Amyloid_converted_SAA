3d633803a318f80e01e8d0f63781c190354f14b1
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-04-25 11:59:02 -0700

    Update helper_functions_parallel.R
    added function for cdrsb bootstrapping

--- helper_functions_parallel.R
+++ helper_functions_parallel.R
@@ -34,6 +34,36 @@ lcmm_bootstrap_ci <- function(new_data, n_iterations, lcmm_data, name_of_biomark
   
 }
 
+lcmm_bootstrap_ci_cdrsb <- function(new_data, n_iterations, lcmm_data, name_of_biomarker) {
+  
+  #prepping for bootstrapping
+  n_sample <- nrow(lcmm_data)
+  n_rows <- nrow(new_data) # newdata for predictions
+  B <- n_iterations
+  boot_pred <- numeric(B) #for the predictions
+
+  #bootstrap
+  registerDoParallel(16)
+  bootstrapped_list_values <- foreach(ind = 1:B) %dopar% {
+    sprintf("%i:%s", ind, name_of_biomarker)
+    set.seed(ind)
+    boot <- sample(n_sample, n_sample, replace = TRUE)
+    
+    lcmm_data$biomarker <- lcmm_data[ , name_of_biomarker]
+    
+    fit.b <- lcmm::lcmm(biomarker ~ adjusted_new_time + age + age*adjusted_new_time + PTGENDER + PTEDUCAT + apoe + CDGLOBAL, #link = c("5-quantsplines"),
+                        random = ~adjusted_new_time, subject="RID", maxiter = 300, data = lcmm_data[boot,], link = "3-equi-splines")
+    
+    boot_pred[ind] <- lcmm::predictY(fit.b, new_data, var.time = "adjusted_new_time", draws = TRUE)
+    boot_pred_fit <- as.data.frame(boot_pred[ind])
+    
+    return(boot_pred_fit)
+  }
+  
+  return(bootstrapped_list_values)
+  
+}
+
 #function to estimate confidence intervals
 confidence_interval <- function(vector, interval) {
   
